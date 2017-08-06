8eb0cf5e8c1d2dfe149f099ca925fc1b5eae2c8b
Reza Alizadeh Majd <r.a.majd@gmail.com>
2017-08-07 01:57:02 +0430

    sidebar translated

--- app/sidebar/sidebar-routes.config.js
+++ app/sidebar/sidebar-routes.config.js
@@ -1,11 +1,11 @@
 "use strict";
 exports.ROUTES = [
-    { path: 'dashboard', title: 'Dashboard', icon: 'dashboard', class: '' },
-    { path: 'user', title: 'User Profile', icon: 'person', class: '' },
-    { path: 'table', title: 'Table List', icon: 'content_paste', class: '' },
-    { path: 'typography', title: 'Typography', icon: 'library_books', class: '' },
-    { path: 'icons', title: 'Icons', icon: 'bubble_chart', class: '' },
-    { path: 'maps', title: 'Maps', icon: 'location_on', class: '' },
-    { path: 'notifications', title: 'Notifications', icon: 'notifications', class: '' },
+    { path: 'dashboard', title: 'داشبورد کاربری', icon: 'dashboard', class: '' },
+    { path: 'user', title: 'پروفایل کاربر', icon: 'person', class: '' },
+    { path: 'table', title: 'جداول', icon: 'content_paste', class: '' },
+    { path: 'typography', title: 'تایپوگرافی', icon: 'library_books', class: '' },
+    { path: 'icons', title: 'آیکن ها', icon: 'bubble_chart', class: '' },
+    { path: 'maps', title: 'نقشه', icon: 'location_on', class: '' },
+    { path: 'notifications', title: 'پیام های هشدار', icon: 'notifications', class: '' },
 ];
 //# sourceMappingURL=sidebar-routes.config.js.map
